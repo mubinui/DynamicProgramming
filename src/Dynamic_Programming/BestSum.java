9a894ceb29a9231836096dbe2b136584d3994689
uicmu <mubin41@student.sust.edu>
2022-08-25 01:38:10 +0600

    BestSum

--- src/Dynamic_Programming/BestSum.java
+++ src/Dynamic_Programming/BestSum.java
@@ -1,23 +1,37 @@
 package Dynamic_Programming;
-
 import java.util.*;
 
 public class BestSum {
-    public static void main(String[] args) {
-        Scanner sc  = new Scanner(System.in);
-        int target = sc.nextInt();
-        int [] numbers = {3,4,5,1,8,9,7};
-        //Dynamic
-        int [] memory = new int[target+1 * 100];
-        Arrays.fill(memory,-100);
-        List<Integer> memo_out = bestSumMemo(target,numbers,memory);
-        System.out.println(memo_out);
-        //Not Dynamic
-        List<Integer> output = bestSum(target,numbers);
-        System.out.println(output);
+    //Dynamic programming BestSum
+    public static ArrayList<Integer> bestSumMemo(int target, int [] numbers, ArrayList<Integer>memoInt,ArrayList<ArrayList<Integer>>memoList){
+        // memoization part
+        for (int i = 0; i <memoInt.size() ; i++) {
+            if(memoInt.get(i)==target){
+                return memoList.get(i);
+            }
+        }
+        // Recursive part
+        if(target == 0) return new ArrayList<>();
+        if(target<0) return null;
 
+        ArrayList<Integer> shortestCombination = null;
+        for (int i = 0; i <numbers.length ; i++) {
+            int reminder = target - numbers[i];
+            ArrayList<Integer> currentCombination = bestSumMemo(reminder,numbers,memoInt,memoList);
+            if(currentCombination!=null){
+                currentCombination.add(numbers[i]);
+                if(shortestCombination==null || shortestCombination.size()>currentCombination.size()){
+                    shortestCombination = new ArrayList<>();
+                    shortestCombination.addAll(currentCombination);
+                }
+            }
 
+        }
+        memoInt.add(target);
+        memoList.add(shortestCombination);
+        return shortestCombination;
     }
+
 
 
     // without Dynamic programming
@@ -33,49 +47,23 @@ public class BestSum {
                 if (minLength==null || current.size()<minLength.size()){
                     minLength = new ArrayList<>();
                     minLength.addAll(current);
-
                 }
-
             }
-
         }
         return minLength;
     }
-    // Memoization
-    public static List<Integer> bestSumMemo (int target, int [] numbers, int [] memory){
-        if (target==0) return new ArrayList<>();//base case
-        if (target<0) return null; // base case
-        List<Integer> minLength = null;
-        for (int i = 0; i <numbers.length ; i++) {
-            int reminder = target - numbers[i];
-            List <Integer> current = new ArrayList<>();
-            if(reminder>=0 && memory[reminder]!=-100){
-                List<Integer> temp = bestSumMemo(reminder,numbers,memory);
-                if (temp!=null){
-                    memory[reminder] = numbers[i];
-                    current.add(numbers[i]);
-                    if (minLength==null || current.size()<minLength.size()){
-                        minLength = new ArrayList<>();
-                        minLength.addAll(current);
-
-                    }
-                }
-            }
-            if (reminder>=0 && memory[reminder]!=-100){
-                current.add(numbers[i]);
-                if (minLength==null || current.size()<minLength.size()){
-                    minLength = new ArrayList<>();
-                    minLength.addAll(current);
-
-                }
-
-
-            }
-
-
-        }
-        return minLength;
-
-
+    public static void main(String[] args) {
+        Scanner sc  = new Scanner(System.in);
+        int target = sc.nextInt();
+        int [] numbers = {3,4,5,1,8,9,7};
+        // Dynamic
+        ArrayList<Integer>lst = new ArrayList<>();
+        ArrayList<ArrayList<Integer>>lstOflst = new ArrayList<>();
+        ArrayList<Integer> memOut = bestSumMemo(target,numbers,lst,lstOflst);
+        System.out.println(memOut);
+        // Non-dynamic
+        List<Integer> output = bestSum(target,numbers);
+        System.out.println(output);
     }
+
 }
