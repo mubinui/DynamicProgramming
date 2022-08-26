033396525a29313a235329ba91ccb7afc89beda1
uicmu <mubin41@student.sust.edu>
2022-08-27 01:16:16 +0600

    Can Construct

--- src/Dynamic_Programming/WordBank/canConstruct.java
+++ src/Dynamic_Programming/WordBank/canConstruct.java
@@ -1,12 +1,18 @@
 package Dynamic_Programming.WordBank;
 
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class canConstruct {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String target = sc.next();
-        String [] wordBank = {"ab", "abc", "cd", "def", "abcd"};
+        String [] wordBank = {"e","ee","eee","eeee","eeeeee"};
+        //Dynamic Programming
+        HashMap<String,Boolean> memory = new HashMap<>();
+        System.out.println(canConstructMemo(target,wordBank,memory));
+
+        // Without memoization
         System.out.println(canConstruct(target,wordBank));
 
     }
@@ -25,8 +31,29 @@ public class canConstruct {
         return false;
     }
 
-    public static boolean canConstructMemo(String target, String [] wordBank){
+    public static boolean canConstructMemo(String target, String [] wordBank, HashMap<String,Boolean>memory){
+        if (memory.containsKey(target)){
+            if(memory.get(target)){
+                System.out.println("->>>>>>");
+                return true;
+            }
+            else {
+                return false;
+            }
+        }
+        if (target.length()==0) return true;
 
+        for (int i = 0; i <wordBank.length ; i++) {
+
+            if(target.length()>=wordBank[i].length() && target.startsWith(wordBank[i])){
+                String tempTarget = target.substring(wordBank[i].length());
+                if(canConstructMemo(tempTarget,wordBank,memory)){
+                    memory.put(tempTarget,true);
+                    return true;
+                }
+            }
+        }
+      memory.put(target,false);
       return false;
     }
 }
--- src/Necessary_Concepts/NecessaryString.java
+++ src/Necessary_Concepts/NecessaryString.java
@@ -9,11 +9,16 @@ public class NecessaryString {
 //        if (s.equals(m)){
 //            System.out.println("True"+s);
 //        }
-        String x = "Mubin";
-        String y = "Mu";
-        if (x.substring(0,y.length()).equals(y)){
-            x = x.substring(y.length());
-            System.out.println(x);
+//        String x = "Mubin";
+//        String y = "Mu";
+//        if (x.substring(0,y.length()).equals(y)){
+//            x = x.substring(y.length());
+//            System.out.println(x);
+//        }
+
+        Boolean memory [] = new Boolean[10];
+        for(Boolean x : memory){
+            System.out.print(x+" ");
         }
     }
 }
