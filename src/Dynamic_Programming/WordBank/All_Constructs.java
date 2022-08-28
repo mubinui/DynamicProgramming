c9623e6a2d70f2790ea04c72551bed66ae1f7778
uicmu <mubin41@student.sust.edu>
2022-08-28 23:01:12 +0600

    All  Construct

---
+++ src/Dynamic_Programming/WordBank/All_Constructs.java
@@ -0,0 +1,17 @@
+package Dynamic_Programming.WordBank;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class All_Constructs {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+    }
+
+    public ArrayList<ArrayList<String>> allConstructs(String target, String [] wordBank) {
+
+    }
+
+    }
+}
