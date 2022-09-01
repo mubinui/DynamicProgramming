8c03760323b66df4500ed11e9f5a389f76d902a4
uicmu <mubin41@student.sust.edu>
2022-09-01 17:48:06 +0600

    All  Construct

--- src/Dynamic_Programming/WordBank/All_Constructs.java
+++ src/Dynamic_Programming/WordBank/All_Constructs.java
@@ -6,12 +6,30 @@ import java.util.Scanner;
 public class All_Constructs {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        String target = sc.next();
+        String [] wordBank = {"ab","abc","cd","def","abcd"};
+        String [] wordBank1 = {"e","ee","eee","eeee"};
+        ArrayList<ArrayList<String>>allCon = new ArrayList<>();
+        ArrayList<ArrayList<String>> constructs = allConstructs(target,wordBank,allCon);
+        System.out.println(constructs);
 
     }
 
-    public ArrayList<ArrayList<String>> allConstructs(String target, String [] wordBank) {
+    public static ArrayList<ArrayList<String>> allConstructs(String target, String [] wordBank,ArrayList<ArrayList<String>>allCon) {
+        if (target.length()==0) return new ArrayList<>();
+        ArrayList<String> construction = new ArrayList<>();
+        for (int i = 0; i <wordBank.length; i++) {
+            if(target.length()>=wordBank[i].length() && target.startsWith(wordBank[i])){
+                String tempTarget = target.substring(wordBank[i].length());
+                if( allConstructs(tempTarget,wordBank,allCon)!=null){
+                    System.out.println(wordBank[i]);
 
+                }
+            }
+
+        }
+        return allCon;
     }
 
-    }
+
 }
